72963e3a1d570d52ee3b7793ef970af43bc446fe
AndreiGNT <aagirnet@gmail.com>
2025-09-10 00:08:09 +0300

    Removing the WebRazor mini project from the Project and created two new Models : OrderHeader and Order Detail

--- BulkyWebRazor_Temp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PageNestWebRazor_Temp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- PageNest.DataAccess/Migrations/20250909210145_addOrderHeadersAndDetailsToDb.Designer.cs
+++ PageNest.DataAccess/Migrations/20250909210145_addOrderHeadersAndDetailsToDb.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PageNest.DataAccess.Data;
 
@@ -11,9 +12,11 @@ using PageNest.DataAccess.Data;
 namespace PageNest.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250909210145_addOrderHeadersAndDetailsToDb")]
+    partial class addOrderHeadersAndDetailsToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
